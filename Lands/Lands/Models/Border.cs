7e7164a54e682e1856a9dd35de4420b32fa49737
Manuel Antonio Batres Luna <al155192@edu.uaa.mx>
2019-11-17 05:28:09 -0600

    u.u

---
+++ Lands/Lands/Models/Border.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lands.Models
+{
+    public class Border
+    {
+        public string Code {
+            get;
+            set;
+        }
+
+        public string Name  {
+            get;
+            set;
+        }
+    }
+}
--- Lands/Lands/ViewModels/MainViewModel.cs
+++ Lands/Lands/ViewModels/MainViewModel.cs
@@ -1,7 +1,16 @@
 ﻿namespace Lands.ViewModels
 {
+    using Lands.Models;
+    using System.Collections.Generic;
     public class MainViewModel
     {
+        #region Properties
+        public List<Land> LandsList {
+            get;
+            set;
+        }
+        #endregion
+
         #region ViewModels
         public LoginViewModel Login
         {
